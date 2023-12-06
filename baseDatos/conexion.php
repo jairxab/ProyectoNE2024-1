c046d0d02218c115ae2fd4a5fc0fa5fff1f87662
Jair Betancourt <jair.xab@gmail.com>
2023-12-05 22:56:42 -0600

    Se organizó la carpeta principal

--- baseDatos/conexion.php
+++ baseDatos/conexion.php
@@ -2,7 +2,7 @@
   $host = 'localhost';
   $usuario = 'root';
   $contraseña = 'Equipo2NE';
-  $base_de_datos = 'BDA_Equipo2';
+  $base_de_datos = 'BDAequipo2';
 
   $conexion = new mysqli($host, $usuario, $contraseña, $base_de_datos);
 
